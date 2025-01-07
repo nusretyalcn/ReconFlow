0c4f1c47c277212e1e96f855a7b530a8d6ddb80f
nusretyalcn <yalcnusret@gmail.com>
2025-01-08 00:59:20 +0300

    DbContext Added with EntityframeworkCore

---
+++ DataAccess/Concrete/EntityFramework/Context/EfDbContext.cs
@@ -0,0 +1,32 @@
+using Core.Entities.Concrete;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete.EntityFramework.Context;
+
+public class EfDbContext:DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=localhost;
+                                          Database=eReconciliationDb;
+                                          User Id= SA;
+                                          Password=1245a22521A;
+                                          TrustServerCertificate=True;
+                                          Encrypt=false");
+    }
+
+    public DbSet<AccountReconciliation> AccountReconciliations { get; set; }
+    public DbSet<AccountReconciliationDetail> AccountReconciliationDetails { get; set; }
+    public DbSet<BaBsReconciliation> BaBsReconciliations { get; set; }
+    public DbSet<BaBsReconciliationDetail> BaBsReconciliationDetails { get; set; }
+    public DbSet<Company> Companies { get; set; }
+    public DbSet<Currency> Currencies { get; set; }
+    public DbSet<CurrencyAccount> CurrencyAccounts { get; set; }
+    public DbSet<MailParameter> MailParameters { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<UserCompany> UserCompanies { get; set; }
+    public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+    public DbSet<OperationClaim> OperationClaims { get; set; }
+    
+}
